25116c22c1fe003b86117c050f7220508257af95
Marpol96 <51094297+Marpol96@users.noreply.github.com>
2020-06-04 00:18:11 -0600

    Afinando detalles del commit del dia 3/6/2020

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 //importar express router
-const express=require("express");
+const express = require("express");
 const routes = express.Router();
 
 //importar controladores
-const proyectosController =require("../controllers/proyectosController");
+const proyectosController = require("../controllers/proyectosController");
 //construir rutas disponibles para el servidor, las rutas deben 
 //exportarse para porder ser utilizadas en otros archivos
-module.exports = function(){
-    routes.get("/",proyectosController.home);
+module.exports = function() {
+    routes.get("/", proyectosController.home);
     routes.post("/nuevo_proyecto", proyectosController.nuevoProyecto);
     return routes;
 }
